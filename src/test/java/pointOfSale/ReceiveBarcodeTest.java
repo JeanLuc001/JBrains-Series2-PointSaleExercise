dafa65cbc9217c191ebb188998151240721b7f05
JeanLuc001 <sebastian.kramer@web.de>
2018-08-12 09:07:12 +0200

    Refactored ReceiveBarcodeTest

--- src/test/java/pointOfSale/ReceiveBarcodeTest.java
+++ src/test/java/pointOfSale/ReceiveBarcodeTest.java
@@ -4,42 +4,49 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
 public class ReceiveBarcodeTest
 {
+	private Checkout sut;
+	private Display disp;
+
+	@Before
+	public void init()
+	{
+		disp = mock(Display.class);
+		sut = new Checkout(disp);
+	}
+
+	private ArgumentCaptor<String> captureDisplayedMessage(String code)
+	{
+		ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
+		sut.onBarcode(code);
+		verify(disp).show(argument.capture());
+		return argument;
+	}
+
 	@Test
 	public void when_empty_barcodestring_then_display_error_message() throws Exception
 	{
-		Display disp = mock(Display.class);
-		ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
-		Checkout sut = new Checkout(disp);
-		sut.onBarcode("");
-		verify(disp).show(argument.capture());
+		ArgumentCaptor<String> argument = captureDisplayedMessage("");
 		assertEquals("ERROR: invalid bar code", argument.getValue());
 	}
 
 	@Test
 	public void when_barcodestring_is_null_then_display_error_message()
 	{
-		Display disp = mock(Display.class);
-		ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
-		Checkout sut = new Checkout(disp);
-		sut.onBarcode(null);
-		verify(disp).show(argument.capture());
+		ArgumentCaptor<String> argument = captureDisplayedMessage(null);
 		assertEquals("ERROR: invalid bar code", argument.getValue());
 	}
 
 	@Test
 	public void when_barcodestring_has_wrong_format_then_display_error_message()
 	{
-		Display disp = mock(Display.class);
-		ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
-		Checkout sut = new Checkout(disp);
 		String invalidBarcode = "124a45";
-		sut.onBarcode(invalidBarcode);
-		verify(disp).show(argument.capture());
+		ArgumentCaptor<String> argument = captureDisplayedMessage(invalidBarcode);
 		assertEquals("ERROR: " + invalidBarcode + " is an invalid bar code", argument.getValue());
 	}
 }
