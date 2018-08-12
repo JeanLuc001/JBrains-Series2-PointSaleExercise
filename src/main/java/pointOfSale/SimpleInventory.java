79a97866c20139dcbbb79bfab383a2609e25346c
JeanLuc001 <sebastian.kramer@web.de>
2018-08-12 15:16:09 +0200

    SimpleInventory: Products can be added and their prices can be queried.

--- src/main/java/pointOfSale/SimpleInventory.java
+++ src/main/java/pointOfSale/SimpleInventory.java
@@ -1,20 +1,27 @@
 package pointOfSale;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class SimpleInventory implements Inventory
 {
-	private Price price;
+	private final Map<String, Price> priceByCode;
+
+	public SimpleInventory()
+	{
+		priceByCode = new HashMap<>();
+	}
 
 	@Override
 	public Optional<Price> getProductPriceFromCode(String code)
 	{
-		return Optional.of(price);
+		return Optional.ofNullable(priceByCode.get(code));
 	}
 
 	@Override
 	public void addProduct(String code, Price price)
 	{
-		this.price = price;
+		priceByCode.put(code, price);
 	}
 }
--- src/test/java/pointOfSale/SimpleInventoryTest.java
+++ src/test/java/pointOfSale/SimpleInventoryTest.java
@@ -1,18 +1,48 @@
 package pointOfSale;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SimpleInventoryTest
 {
+	Inventory sut;
+	
+	@Before
+	public void init()
+	{
+		sut = new SimpleInventory();
+	}
+	
 	@Test
 	public void when_adding_product_then_price_available() throws Exception
 	{
-		Inventory sut = new SimpleInventory();
 		Price price = new Price("55.73");
 		String code = "00258288";
 		sut.addProduct(code, price);
 		assertEquals(price, sut.getProductPriceFromCode(code).get());
+	}
+	
+	@Test
+	public void when_adding_two_products_then_prices_are_available() throws Exception
+	{
+
+		Price price1 = new Price("55.73");
+		String code1 = "00258288";
+		Price price2 = new Price("4.3");
+		String code2 = "1111111";
+		sut.addProduct(code1, price1);
+		sut.addProduct(code2, price2);
+		assertEquals(price1, sut.getProductPriceFromCode(code1).get());
+		assertEquals(price2, sut.getProductPriceFromCode(code2).get());
+	}
+	
+	@Test
+	public void when_querying_prize_for_non_existing_product_then_return_empty_Optional() throws Exception
+	{
+		String code = "02258288";
+		assertFalse(sut.getProductPriceFromCode(code).isPresent());
 	}
 }
